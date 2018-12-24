90b7a6860e59585ffa910a772a2bcaa949355be7
mingsec <mingsec@outlook.com>
2018-12-24 15:46:26 +0800

    增加新建销售订单页面

--- accounting/forms.py
+++ accounting/forms.py
@@ -2,38 +2,38 @@
 # @Author: ZhuZefeng
 # @Date:   2018-12-21 15:22:57
 # @Last Modified by:   ZhuZefeng
-# @Last Modified time: 2018-12-21 21:29:52
+# @Last Modified time: 2018-12-22 17:23:21
 
 
-from django  import forms
+from django       import forms
 from django.forms import widgets
-from .models import Order
+from .models      import Order
 
-class OrderForm(forms.Form):
+class OrderForm(forms.ModelForm):
     """用于创建销售订单的表单"""
-
-    '''
+    
     class Meta:
         model  = Order
         fields = ['number_of_purchase_contract', 
                     'number_of_client', 
-                    'product',
-                    'date_of_sale',
-                    'service_life',
-                    'sales_amount',
-                    'tax_included_or_not',
-                    'invoiced_or_not',
-                    'number_of_invoiced',
+                    'product', 
+                    'date_of_sale', 
+                    'service_life', 
+                    'sales_amount', 
+                    'tax_included_or_not', 
+                    'invoiced_or_not', 
+                    'number_of_invoiced', 
                     'closed_or_not',]
-    '''
 
-    number_of_purchase_contract = forms.CharField(label='订单编号', widget=forms.TextInput({'placeholder': '请输入订单编号'}))
-    number_of_client            = forms.CharField(label='客户名称', widget=forms.TextInput({'placeholder': '请输入客户名称'}))
-    product                     = forms.CharField(label='产品名称', widget=forms.TextInput({'placeholder': '请输入产品名称'}))
-    date_of_sale                = forms.DateField(label='销售日期', widget=forms.TextInput({'type':'data', 'placeholder': '请输入销售日期'}))
-    service_life                = forms.IntegerField(label='使用期限', widget=forms.TextInput({'placeholder': '请输入使用期限'}))
-    sales_amount                = forms.FloatField(label='销售金额', widget=forms.TextInput({'placeholder': '请输入销售金额'}))
-    tax_included_or_not         = forms.BooleanField(label='是否含税', widget=forms.Select(choices=((0,'不含增值税'),(1,'含增值税'))))
-    invoiced_or_not             = forms.BooleanField(label='是否开票', widget=forms.Select(choices=((0,'未开发票'),(1,'已开发票'))))
-    number_of_invoiced          = forms.CharField(label='发票编号', widget=forms.TextInput({'placeholder': '请输入发票编号'}))
-    closed_or_not               = forms.BooleanField(label='是否到期', widget=forms.Select(choices=((0,'未到期'),(1,'已到期'))))
+    '''
+    number_of_purchase_contract = forms.CharField(label='订单编号')
+    number_of_client            = forms.CharField(label='客户名称')
+    product                     = forms.CharField(label='产品名称')
+    date_of_sale                = forms.DateField(label='销售日期')
+    service_life                = forms.IntegerField(label='使用期限')
+    sales_amount                = forms.FloatField(label='销售金额')
+    tax_included_or_not         = forms.IntegerField(label='是否含税', widget=forms.Select(choices=(('0','不含增值税'),('1','含增值税'))))
+    invoiced_or_not             = forms.IntegerField(label='是否开票', widget=forms.Select(choices=(('0','未开发票'),('1','已开发票'))))
+    number_of_invoiced          = forms.CharField(label='发票编号')
+    closed_or_not               = forms.IntegerField(label='是否到期', widget=forms.Select(choices=(('0','未到期'),('1','已到期'))))
+    '''
--- accounting/models.py
+++ accounting/models.py
@@ -11,10 +11,10 @@ class Order(models.Model):
     date_of_sale                = models.DateField()
     service_life                = models.IntegerField()
     sales_amount                = models.FloatField()
-    tax_included_or_not         = models.BooleanField(default=False)
-    invoiced_or_not             = models.BooleanField(default=False)
+    tax_included_or_not         = models.BooleanField(choices=((False,'不含增值税'),(True,'含增值税')), default=False)
+    invoiced_or_not             = models.BooleanField(choices=((False,'未开发票'),(True,'已开发票')), default=False)
     number_of_invoiced          = models.CharField(max_length=20)
-    closed_or_not               = models.BooleanField(default=False)
+    closed_or_not               = models.BooleanField(choices=((False,'未到期'),(True,'已到期')), default=False)
 
     def __str__(self):
         """返回销售订单编号"""
